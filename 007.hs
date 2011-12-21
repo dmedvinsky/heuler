e8839c2c967d0b6b88abe8972a7ea8d39fc6197f
Dmitry Medvinsky <dmedvinsky@gmail.com>
2011-12-21 09:51:06 +0400

    Problem 7

---
+++ 007.hs
@@ -0,0 +1,15 @@
+module Main
+where
+
+
+primes = 2 : filter ((== 1) . length . primeFactors) [3, 5 ..]
+
+primeFactors n = factorize n primes
+  where factorize n primes@(p:ps)
+          | p * p > n      = [n]
+          | n `mod` p == 0 = p : (factorize (n `div` p) primes)
+          | otherwise      = factorize n ps
+
+solve = primes !! 10000
+
+main = print solve
