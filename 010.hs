2bfba9244b72b873dfbc5009e2218b47d07e1f1c
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-01-01 00:47:43 +0400

    Factor out primes functions

--- 010.hs
+++ 010.hs
@@ -1,14 +1,8 @@
 module Main
 where
 
+import Primes
 
-primes = 2 : filter ((== 1) . length . primeFactors) [3, 5 ..]
-
-primeFactors n = factorize n primes
-  where factorize n primes@(p:ps)
-          | p * p > n      = [n]
-          | n `mod` p == 0 = p : (factorize (n `div` p) primes)
-          | otherwise      = factorize n ps
 
 solve = sum $ takeWhile (< 2000000) primes
 
